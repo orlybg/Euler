f2ee20ff88fb5a9558bbf7781d25f344642d0d10
Orlando Briceno Gomez <orlybg@in.com>
2011-06-08 09:34:14 -0700

    more methods for problem2

--- 002/fibo.rb
+++ 002/fibo.rb
@@ -4,8 +4,33 @@
 
 @phi = (1 + Math.sqrt(5)) / 2 
 
+# binet formula approach, floor call for rounding fails on huge numbers
+# but is a lot faster
 def binet(n)
 	return ((@phi**n  - (1 - @phi)**n ) /   Math.sqrt(5) ).floor
+end
+
+# classic recursive approach
+def fib_rec(n)
+	n < 2 ? n : fib_rec(n-1) + fib_rec(n-2)
+end
+
+# iterative approach, verbose but faster than the recursive one
+def fib_iter(n)
+	a   = 0
+	b   = 1
+	aux = 0
+
+	if (n == 0)
+        	return n
+	end
+
+	(n-1).times do
+		aux = b
+		b   = a + b
+		a   = aux 
+	end
+	return b
 end
 
 n     = 0
@@ -18,4 +43,5 @@ while (@fibn < 4000000)
 		@sum += @fibn
 	end
 end
+
 puts @sum

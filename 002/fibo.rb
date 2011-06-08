d9fbeafb807a1311767f69d3993b4f47ccc92092
Orlando Briceno Gomez <orlybg@in.com>
2011-06-07 18:32:48 -0700

    foo

--- 002/fibo.rb
+++ 002/fibo.rb
@@ -2,21 +2,20 @@
 # http://projecteuler.net/index.php?section=problems&id=2
 # Orlando Briceno 
 
-
 @phi = (1 + Math.sqrt(5)) / 2 
 
-def fibo(n)
+def binet(n)
 	return ((@phi**n  - (1 - @phi)**n ) /   Math.sqrt(5) ).floor
 end
 
-
-for n in 1..4000000
-	sum  = 0
-	fibn =  fibo(n)
-	if (fibn % 2 == 0) 
-		sum += fibn
+n     = 0
+@sum  = 0
+@fibn = 0
+while (@fibn < 4000000)
+        n = n + 1
+	@fibn = binet(n)
+	if (@fibn % 2 == 0) 
+		@sum += @fibn
 	end
-
 end
-
-puts sum
+puts @sum
